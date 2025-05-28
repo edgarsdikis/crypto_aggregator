a9562fb45bf6a061ecc2366707247accc9439c6e
Edgars Dikis <edgarsdikis@startschool.org>
2025-05-29 00:26:46 +0300

    Remove migration files

--- apps/integrations/coinmarketcap/client.py
+++ apps/integrations/coinmarketcap/client.py
@@ -1,6 +1,5 @@
 import requests
 from django.conf import settings
-
 from .exceptions import (CoinMarketCapApiError, CoinMarketCapConnectionError,
                          CoinMarketCapError, CoinMarketCapRateLimitError)
 
