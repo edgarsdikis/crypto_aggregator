41ac4f4956582ee564d2546b08316451dc3e8500
Edgars Dikis <edgarsdikis@startschool.org>
2025-05-29 03:04:39 +0300

    Add list and market methods serializers

---
+++ apps/integrations/coingecko/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+class CoinGeckoCoinsListSerializer(serializers.Serializer):
+    """
+    Serializer for individual coin from CoinGecko coins/list API
+    """
+
+    id = serializers.CharField()
+    platforms = serializers.DictField(required=False, default=dict)
+
+    def get_chain_contracts(self, obj):
+        """
+        Extract contract addresses per chain from platforms data
+        Returns: dict like {'ethereum': '0x123...', 'bsc': '0x456...'}
+        """
+        platforms = obj.get("platforms", {})
+        return platforms
+
+
+class CoinGeckoMarketDataSerializer(serializers.Serializer):
+    """
+    Serializer for individual coin from CoinGecko coins/markets API
+    """
+
+    id = serializers.CharField()
+    symbol = serializers.CharField()
+    name = serializers.CharField()
+    image = serializers.URLField()
+    current_price = serializers.DecimalField(max_digits=30, decimal_places=12, allow_null=True)
+    market_cap_rank = serializers.IntegerField(allow_null=True)
+    last_updated = serializers.DateTimeField()
