38deee27b7caf1b34711d462d40b235ad551e182
Edgars Dikis <edgarsdikis@startschool.org>
2025-07-07 14:39:49 +0300

    Update start.sh
    Update script to run all of the processes in one container

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Exit on any failure
 set -e
 
 echo "Running database migrations..."
@@ -8,6 +5,12 @@ python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Starting Celery worker in background..."
+celery -A config worker -l INFO --detach
+
+echo "Starting Celery beat in background..."
+celery -A config beat -l INFO --detach
 
 echo "Starting server..."
 exec gunicorn --bind 0.0.0.0:$PORT config.wsgi:application
