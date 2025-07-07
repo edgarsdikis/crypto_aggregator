7a7b52dd570f1c520ae45ac45eece005089654ab
Edgars Dikis <edgarsdikis@startschool.org>
2025-07-07 15:13:56 +0300

    Automate super_user creation

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,9 @@ set -e
 echo "Running database migrations..."
 python manage.py migrate --noinput
 
+echo "Ensuring superuser exists..."
+python manage.py ensure_superuser  # Only creates if not local
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
